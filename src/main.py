d12f1c38a51ca781050a3fd4e22ed7cd5c640115
Roman Kharitonov <refaim.vl@gmail.com>
2010-09-17 18:01:47 +1100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+
+import sys
+
+def main(argv):
+    if len(argv) != 1:
+        print('Usage: main.py <testdir>')
+        return 1
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
