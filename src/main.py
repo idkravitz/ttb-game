1eb3c1a5460ec0dfa1a73e0a9a82634bedd3aa43
Roman Kharitonov <refaim.vl@gmail.com>
2010-09-17 21:33:57 +1100

    Add JSON load function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 ﻿# -*- coding: utf-8 -*-
 
 import sys
+import json
+
+def load_json(filename):
+    find_next = lambda s, pos: json.decoder.WHITESPACE.match(s, pos).end()
+    text = open(filename).read()
+    pos, end = 0, len(text)
+    result = []
+    while pos != end:
+        pos = find_next(text, pos)
+        request, pos = json._default_decoder.raw_decode(text, idx=pos)
+        result.append(request)
+    return result
 
 def main(argv):
     if len(argv) != 1:
