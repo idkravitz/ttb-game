ac277f965fd5d45c13331e33e44e7b7ebec33cb6
Roman Kharitonov <refaim.vl@gmail.com>
2010-09-18 00:32:30 +1100

    Add function which pre-parses the request

--- src/main.py
+++ src/main.py
@@ -2,8 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
+
+def parse_request(request):
+    result = lambda status: json.dumps({'result': status})
+    try:
+        request = json.loads(request)
+        if not isinstance(request, dict) or not len(request):
+            return result('badRequest')
+        # call process_request
+    except ValueError:
+        return result('badRequest')
 
 def main(argv):
+    print(parse_request(input()))
     return 0
 
 if __name__ == '__main__':
