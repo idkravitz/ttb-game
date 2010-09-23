7f1351a6b05eb80a70d013c45bbaf8963863c0c0
Roman Kharitonov <refaim.vl@gmail.com>
2010-09-23 12:21:47 +1100

    Tester: hide successful tests by default, add debug options

--- src/test.py
+++ src/test.py
@@ -5,6 +5,7 @@ import sys
 import os
 import glob
 import json
+import optparse
 from main import parse_request
 from db import db_instance as dbi
 
@@ -35,27 +36,48 @@ def compare(testname):
     else:
         return 'NO ANSWER'
 
-def main(argv):
-    if len(argv) != 1:
-        return error('Usage: test.py <testdir>')
-    testdir = argv[0]
-    if not os.path.exists(testdir) or not os.path.isdir(testdir):
-        return error('Directory not found')
+def launch(test, debug=False, verbose=False):
+    testname = os.path.splitext(os.path.normpath(test))[0]
+    requests = load_json(test)
+    try:
+        oldout = sys.stdout
+        with open(testname + '.out', 'w') as sys.stdout:
+            for request in requests:
+                print(parse_request(request))
+    finally:
+        sys.stdout = oldout
+        dbi().clear()
 
-    for dirpath, dirnames, filenames in os.walk(testdir):
-        for test in glob.iglob(os.path.join(dirpath, '*.tst')):
-            testname = os.path.splitext(os.path.normpath(test))[0]
-            requests = load_json(test)
-            try:
-                oldout = sys.stdout
-                with open(testname + '.out', 'w') as sys.stdout:
-                    for request in requests:
-                        print(parse_request(request))
-            finally:
-                sys.stdout = oldout
-                print('Test {0} {1}'.format(test.replace(testdir, ''), compare(testname)))
-                dbi().clear()
+    result = compare(testname)
+    if result != 'OK' or verbose or debug:
+        print('Test {0} {1}'.format(os.path.normpath(test), result))
+    if debug:
+        print(open(testname + '.out').read())
+
+def main():
+    parser = optparse.OptionParser(usage='test.py [options] <test(s)>')
+    parser.disable_interspersed_args()
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
+        default=False, help='show successful tests')
+    parser.add_option('-d', '--debug', action='store_true', dest='debug',
+        default=False, help='show tests output (includes --verbose)')
+    try:
+        (options, args) = parser.parse_args()
+    except optparse.OptionError as e:
+        return error(e.msg)
+
+    if not len(args):
+        return error(parser.format_help())
+    for arg in args:
+        if not os.path.exists(arg):
+            return error('Path not found: {0}'.format(arg))
+        if os.path.isdir(arg):
+            for dirpath, dirnames, filenames in os.walk(arg):
+                for test in glob.iglob(os.path.join(dirpath, '*.tst')):
+                    launch(test, debug=options.debug, verbose=options.verbose)
+        else:
+            launch(arg, debug=options.debug, verbose=options.verbose)
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
