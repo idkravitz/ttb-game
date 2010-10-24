761d0c13a47596bc1a945c186ed81130b4e40042
Dmitry S. Kravtsov <idkravitz@gmail.com>
2010-10-25 01:46:56 +1100

    Change some server behaviour, new ajax detection

--- src/server.py
+++ src/server.py
@@ -39,34 +39,49 @@ class Application(object):
     def __call__(self, environ, start_response):
         setup_testing_defaults(environ)
         self.env = environ
+        self.callback = start_response
+        self.path = environ['PATH_INFO']
+        if self.env['REQUEST_METHOD'] == 'POST':
+            return self.post()
+        else: # static serve
+            return self.get()
+
+    def get(self):
+        path = get_static(self.path)
+        if path is None:
+            return self.respond_404()
+        else:
+            mtype = mimetypes.guess_type(path)[0]
+            if mtype == 'text/html':
+                mtype += '; charset=utf-8'
+            headers = [(b'Content-type', mtype)]
+            ret = open(path, 'rb')
+            self.callback(self.status, headers)(ret.read())
+            ret.close()
+        return []
+
+    def post(self):
         if self.is_ajax():
-            self.raw = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
+            self.raw = self.env['wsgi.input'].read(int(self.env['CONTENT_LENGTH']))
             self.body = self.raw.decode("utf-8")
             args = { name: value[0] if len(value) == 1 else value
                 for name, value in parse_qs(self.body, keep_blank_values=True).items() }
             self.body = json.dumps(args)
             ret = [ main.parse_request(self.body) ]
-            start_response(self.status, self.html_headers)
-            return ret
-        else: # static serve
-            path = environ['PATH_INFO']
-            path = get_static(path)
-            if path is None:
-                status = b'404 NOT FOUND'
-                start_response(status, self.html_headers)
-                return ["Page not found"]
-            else:
-                mtype = mimetypes.guess_type(path)[0]
-                if mtype == 'text/html':
-                    mtype += '; charset=utf-8'
-                headers = [(b'Content-type', mtype)]
-                ret = open(path, 'rb')
-                start_response(self.status, headers)(ret.read())
-            return []
+            self.callback(self.status, self.html_headers)
+        else:
+            ret = self.respond_404()
+        return ret
 
     def is_ajax(self):
-        key, val = 'HTTP_X_REQUESTED_WITH', 'XMLHttpRequest'
-        return key in self.env and self.env[key] == val
+        return self.path == '/'
+
+    def respond_404(self):
+        status = b'404 NOT FOUND'
+        print(self.path)
+        self.callback(status, self.html_headers)
+        return ["Page not found"]
+
 
 httpd = make_server('', port, Application())
 print("Serving on port {0}...".format(port))
