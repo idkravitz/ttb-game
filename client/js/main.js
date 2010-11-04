fdcbbc935a768709cedf5755ca2d68482ae91976
Dmitry S. Kravtsov <idkravitz@gmail.com>
2010-11-04 23:19:44 +1000

    Chat in development state

--- client/js/main.js
+++ client/js/main.js
@@ -11,12 +11,72 @@ function describeSections()
             init: function() {}          // actions to perform after showing
         },
         main: {
-            body: $('#main'),
-            show: [$('#menu-wrapper')],
+            body: $("#main"),
+            show: [$("#menu-wrapper")],
             hide: [],
             init: function() {}
+        },
+        lobby: {
+            body: $("#lobby"),
+            show: [$("#menu-wrapper")],
+            hide: [],
+            init: function()
+            {
+                if(!session)
+                {
+                    showSection("registration");
+                }
+                getJSON({
+                    cmd: "createGame",
+                    sid: session.sid,
+                    gameName: "test",
+                    playersCount: 4,
+                    totalCost: 300,
+                    mapName: "x_2",
+                    factionName: "Headcrabs"
+                },
+                function() {
+                }, function() { joinGame("test"); });
+                getLobbyState();
+            }
         }
     }
+}
+
+function getLobbyState()
+{
+    var command = {cmd: "getChatHistory", sid: session.sid, gameName: "test"};
+    if(sections.lobby.last_id)
+    {
+        $.extend(command, { since: sections.lobby.last_id });
+    }
+    getJSON(command,
+    function(json){
+        if(json.chat.length)
+        {
+            sections.lobby.last_id = json.chat[json.chat.length-1].id;
+            $.each(json.chat, function(i, rec)
+            {
+               var name=$(document.createElement("p")).addClass("chat-name");
+               var message=$(document.createElement("p")).addClass("chat-message");
+               name.text(rec.username);
+               message.text(rec.message);
+               var record=$(document.createElement("div"));
+               record.append(name).append(message);
+               $("#chat").append(record);
+            });
+        }
+        setTimeout("getLobbyState()", 3000);
+    });
+}
+
+function joinGame(gameName)
+{
+    getJSON(
+        {cmd:"joinGame", sid: session.sid, gameName: gameName},
+        function(json){
+        }
+    );
 }
 
 function showSection(section_name)
@@ -114,7 +174,7 @@ function globalAjaxCursorChange()
 function grabForm(form)
 {
     var obj = {};
-    $("input[type!='submit']", form).each(function(i, v)
+    $("input[type!='submit'], textarea", form).each(function(i, v)
     {
         obj[$(v).attr('name')] = $(v).attr('rel') == 'int' ? parseInt($(v).val()) : $(v).val();
     });
@@ -149,6 +209,7 @@ $(document).ready(function()
     describeSections();
     window.onhashchange = innerShowSection;
 
+    session = readCookie("session");
     if(!showSection(getCurrentSection() || "registration"))
     {
         innerShowSection(); // force redraw to avoid artifacts
@@ -163,7 +224,12 @@ $(document).ready(function()
 
         if (form.attr("name") == "registration")
         {
-            var command = { cmd: "register" };
+            command = { cmd: "register" };
+        }
+        else if(form.attr("name") == "message")
+        {
+            command = { cmd: "sendMessage", sid: session.sid, gameName: "test" };
+            alert(data.text);
         }
 
         $(".error", form).hide();
@@ -172,13 +238,15 @@ $(document).ready(function()
             $.extend(data, command),
             function (json)
             {
-                session = setCookie("session",
-                    {
-                        sid: json.sid,
-                        username: data.username
-                    }
-                );
-                showSection("main");
+                if(form.attr("name") == "registration") {
+                    session = setCookie("session",
+                        {
+                            sid: json.sid,
+                            username: data.username
+                        }
+                    );
+                    showSection("main");
+                }
             },
             function (message) { formError(form, message); }
         );
