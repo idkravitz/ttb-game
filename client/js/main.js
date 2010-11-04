67e419bc5a0dafd081c8d69f337ef219642f0b71
Roman Kharitonov <refaim.vl@gmail.com>
2010-11-05 02:43:14 +1000

    Reorder functions

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,87 @@
 var sections; // descriptions of toplevel sections (which behave like pages)
 var session;  // session info, obtained from cookies
 
+function setCookie(name, data)
+{
+    $.cookie(name, JSON.stringify(data));
+    return data;
+}
+
+function readCookie(name)
+{
+    return JSON.parse($.cookie(name));
+}
+
+function updateCookie(name, data)
+{
+    return setCookie(name, $.extend(readCookie(name), data));
+}
+
+function getJSON(data, handler, error_handler)
+{
+    $.getJSON(
+        "/ajax",
+        { data: JSON.stringify(data) },
+        function (json)
+        {
+            if (json.status == 'ok')
+            {
+                handler(json);
+            }
+            else
+            {
+                if (error_handler == null)
+                    error_handler = alert;
+                error_handler(json.message);
+            }
+        }
+    );
+}
+
+function globalAjaxCursorChange()
+{
+    $("html").bind("ajaxStart", function()
+    {
+        $(this).addClass('busy');
+    }).bind("ajaxStop", function()
+    {
+        $(this).removeClass('busy');
+    });
+}
+
+
+function showSection(section_name)
+{
+    if (getCurrentSection() == section_name)
+    {
+        return false;
+    }
+    window.location.hash = section_name;
+    return true;
+}
+
+function getCurrentSection()
+{
+    return window.location.hash.substr(1); // remove # symbol
+}
+
+function innerShowSection()
+{
+    var section_name = getCurrentSection();
+    if (!section_name || !(section_name in sections))
+    {
+        window.location.hash = "registration";
+        return;
+    }
+
+    section = sections[section_name];
+    $("#content > section").hide();
+    section.init();
+    section.body.show();
+    $.each(section.show, function(i, v) { v.show(); });
+    $.each(section.hide, function(i, v) { v.hide(); });
+}
+
 function describeSections()
 {
     sections = {
@@ -41,6 +122,22 @@ function describeSections()
             init: initLobby
         }
     }
+}
+
+function initRegistration()
+{
+    $('form[name="registration"]').submit(function()
+    {
+        return submitForm(
+            $(this),
+            function(json, data)
+            {
+                session = setCookie(
+                    'session', { sid: json.sid, username: data.username });
+                showSection("active-games");
+            }
+        );
+    });
 }
 
 function initLobby()
@@ -106,36 +203,81 @@ function getLobbyState()
     });
 }
 
-function showSection(section_name)
+function getGamesList()
 {
-    if (getCurrentSection() == section_name)
-    {
-        return false;
-    }
-    window.location.hash = section_name;
-    return true;
+    getJSON(
+        addSid({ cmd: 'getGamesList' }),
+        function (json)
+        {
+            var table = $('#active-games table');
+            var empty_message = $('#active-games #empty-server');
+            if (!json.games.length)
+            {
+                table.hide();
+                empty_message.show();
+                return;
+            }
+
+            $('tr', table).not($('tr', table).first()).remove();
+            $.each(json.games, function(i, game)
+            {
+                if (game.gameStatus != 'finished')
+                {
+                    var row = document.createElement('tr');
+                    var players = game.connectedPlayersCount + ' / ' + game.playersCount;
+                    var order = [
+                        game.gameName,
+                        game.gameStatus,
+                        players,
+                        game.mapName,
+                        game.factionName,
+                        game.totalCost
+                    ];
+                    $.each(order,
+                        function(j, string){
+                            $(row).append('<td>' + string + '</td>');
+                        }
+                    );
+                    table.append(row);
+                }
+            });
+            empty_message.hide();
+            table.show();
+        }
+    );
+    //setTimeout("getGamesList()", 3000);
 }
 
-function getCurrentSection()
+function initCreateGame()
 {
-    return window.location.hash.substr(1); // remove # symbol
-}
-
-function innerShowSection()
-{
-    var section_name = getCurrentSection();
-    if (!section_name || !(section_name in sections))
+    function updateSelect(command, attr)
     {
-        window.location.hash = "registration";
-        return;
+        getJSON(
+            addSid({ cmd: command }),
+            function (json)
+            {
+                var array = attr + 's';
+                var select = $('#creation-' + attr);
+                select.empty();
+                for (i = 0; i < json[array].length; ++i)
+                {
+                    select.append(
+                       $('<option value="' + i + '">' + json[array][i][attr] + '</option>'));
+                }
+            }
+        );
     }
+    updateSelect('getMapList', 'map');
+    updateSelect('getFactionList', 'faction');
 
-    section = sections[section_name];
-    $("#content > section").hide();
-    section.init();
-    section.body.show();
-    $.each(section.show, function(i, v) { v.show(); });
-    $.each(section.hide, function(i, v) { v.hide(); });
+    $('form[name="creation"]').submit(function()
+    {
+        return submitForm($(this), function(json, data)
+        {
+            session = updateCookie("session", {gameName: data.gameName});
+            showSection("lobby");
+        });
+    });
 }
 
 function initNavigation()
@@ -181,120 +323,6 @@ function initHorzMenu()
     });
 }
 
-function initRegistration()
-{
-    $('form[name="registration"]').submit(function()
-    {
-        return submitForm(
-            $(this),
-            function(json, data)
-            {
-                session = setCookie(
-                    'session', { sid: json.sid, username: data.username });
-                showSection("active-games");
-            }
-        );
-    });
-}
-
-function getGamesList()
-{
-    getJSON(
-        addSid({ cmd: 'getGamesList' }),
-        function (json)
-        {
-            var table = $('#active-games table');
-            var empty_message = $('#active-games #empty-server');
-            if (!json.games.length)
-            {
-                table.hide();
-                empty_message.show();
-                return;
-            }
-
-            $('tr', table).not($('tr', table).first()).remove();
-            $.each(json.games, function(i, game)
-            {
-                if (game.gameStatus != 'finished')
-                {
-                    var row = document.createElement('tr');
-                    var players = game.connectedPlayersCount + ' / ' + game.playersCount;
-                    var order = [
-                        game.gameName,
-                        game.gameStatus,
-                        players,
-                        game.mapName,
-                        game.factionName,
-                        game.totalCost
-                    ];
-                    $.each(order,
-                        function(j, string){
-                            $(row).append('<td>' + string + '</td>');
-                        }
-                    );
-                    table.append(row);
-                }
-            });
-            empty_message.hide();
-            table.show();
-        }
-    );
-    setTimeout("getGamesList()", 3000);
-}
-
-function initCreateGame()
-{
-    function updateSelect(command, attr)
-    {
-        getJSON(
-            addSid({ cmd: command }),
-            function (json)
-            {
-                var array = attr + 's';
-                var select = $('#creation-' + attr);
-                select.empty();
-                for (i = 0; i < json[array].length; ++i)
-                {
-                    select.append(
-                       $('<option value="' + i + '">' + json[array][i][attr] + '</option>'));
-                }
-            }
-        );
-    }
-    updateSelect('getMapList', 'map');
-    updateSelect('getFactionList', 'faction');
-
-    $('form[name="creation"]').submit(function()
-    {
-        return submitForm($(this), function(json, data)
-        {
-            session = updateCookie("session", {gameName: data.gameName});
-            showSection("lobby");
-        });
-    });
-}
-
-function getJSON(data, handler, error_handler)
-{
-    $.getJSON(
-        "/ajax",
-        { data: JSON.stringify(data) },
-        function (json)
-        {
-            if (json.status == 'ok')
-            {
-                handler(json);
-            }
-            else
-            {
-                if (error_handler == null)
-                    error_handler = alert;
-                error_handler(json.message);
-            }
-        }
-    );
-}
-
 function addSid(data)
 {
     return $.extend(data, { sid: session.sid });
@@ -305,54 +333,29 @@ function addGame(data)
     return $.extend(data, { gameName: session.gameName });
 }
 
-function globalAjaxCursorChange()
-{
-    $("html").bind("ajaxStart", function()
-    {
-        $(this).addClass('busy');
-    }).bind("ajaxStop", function()
-    {
-        $(this).removeClass('busy');
-    });
-}
-
-function grabForm(form)
-{
-    var obj = {};
-    $("input[type!='submit'], textarea", form).each(function(i, v)
-    {
-        obj[$(v).attr('name')] = $(v).attr('rel') == 'int' ? parseInt($(v).val()) : $(v).val();
-    });
-    $("select", form).each(function(i, v)
-    {
-        obj[$(v).attr('name')] = $(':selected', v).text();
-    });
-    return obj;
-}
-
-function formError(form, text)
-{
-    form.prepend('<p class="error ui-corner-all">' + '<img src="/images/error.png">' + text + '</p>');
-}
-
-function setCookie(name, data)
-{
-    $.cookie(name, JSON.stringify(data));
-    return data;
-}
-
-function readCookie(name)
-{
-    return JSON.parse($.cookie(name));
-}
-
-function updateCookie(name, data)
-{
-    return setCookie(name, $.extend(readCookie(name), data));
-}
-
 function submitForm(form, handler)
 {
+    function formError(form, text)
+    {
+        form.prepend(
+            '<p class="error ui-corner-all">' +
+            '<img src="/images/error.png">' + text + '</p>');
+    }
+
+    function grabForm(form)
+    {
+        var obj = {};
+        $("input[type!='submit'], textarea", form).each(function(i, v)
+        {
+            obj[$(v).attr('name')] = $(v).attr('rel') == 'int' ? parseInt($(v).val()) : $(v).val();
+        });
+        $("select", form).each(function(i, v)
+        {
+            obj[$(v).attr('name')] = $(':selected', v).text();
+        });
+        return obj;
+    }
+
     var data = grabForm(form);
     var commands = {
         registration: function () { return { cmd: 'register' }; },
