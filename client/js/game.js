caf85d154fcc21fd68a9dde5dfc38894bdfe6114
Olya Lobodina <relloudder@gmail.com>
2010-11-18 23:40:06 +1000

    Change changePos, divide into functions, convert map, fix units arrangement

--- client/js/game.js
+++ client/js/game.js
@@ -4,62 +4,21 @@ function changeCell(th, map)
     var w = document.getElementById("tableMap").rows[0].cells[0];
     var y = Math.round(($(th).position().top - $(fMap).position().top)/$(w).width());
     var x = Math.round(($(th).position().left - $(fMap).position().left)/$(w).width());
-    changePos(x, y, map.length);
+    x = changePos(x, map.length);
+    y = changePos(y, map.length);
     if ((x >= 0) && (x < map.length) && (y >= 0) && (y < map.length))
     {
         if (map[y][x] == 'used') map[y][x] = '1';
     }
 };
 
-function drawMap(map)
+function gameInterface(map,army)
 {
-    var n = map.length;
-    var tableMap = document.getElementById("tableMap");
-    var row = tableMap.insertRow(0);
-    var colorsMap = {
-        'x': '#666666',
-        '1': '#ff6666',
-        '.': '#66cc00',
-        '2': '#6666ff'
-    };
-    for (var i = 0; i < n; i++)
-    {
-        var row = tableMap.insertRow(i);
-        for (var j = 0; j < n; j++)
-        {
-            var cell = row.insertCell(j);
-            cell.style.backgroundColor = colorsMap[map[i][j]];
-        }
-    }
+    map = drawMap(map);
+    unitsGame = [{"count":2,"name":"one"},{"count":1,"name":"two"},{"count":2,"name":"three"}];
+    showUnits(map,unitsGame);
 
-    centerMap();
-
-    var i = 3;
-    fragment = document.createDocumentFragment();
-    div = document.createElement('div');
-    div.className ='factDiv';
-    pic = document.createElement('img');
-    pic.src='images/person1.bmp';
-    div.appendChild(pic);
-    while (i--)
-    {
-           var t = div.cloneNode(true);
-           $(t).mousedown(function(){
-               changeCell(this,map);
-           });
-           fragment.appendChild(t);
-    }
-    pic.src='images/house.bmp';
-    pic.width = 40;
-    i = 3;
-    while (i--)
-    {
-           var t = div.cloneNode(true);
-           $(t).mousedown(function(){
-               changeCell(this,map);
-           });
-           fragment.appendChild(t);
-    }
+    var n = map[0].length;
     $('#control-panel').append(fragment);
 
     $('.factDiv').dblclick(function(){
@@ -80,7 +39,8 @@ function drawMap(map)
             x = Math.round((ui.draggable.offset().top - $(cell).offset().top + 2)/$(cell).width());
             y = Math.round((ui.draggable.offset().left - $(cell).offset().left + 2)/$(cell).width());
             //don't have n cell (n-1 the last)
-            changePos(x, y, n);
+            x = changePos(x, n);
+            y = changePos(y, n);
             newCell = document.getElementById("tableMap").rows[x].cells[y];
       	    posY = $(newCell).offset().top;
 		    if(map[x][y] == '1')
@@ -105,18 +65,72 @@ function drawMap(map)
             ui.draggable.offset({top:posY+4, left:posX+4});
         }
     });
-
 };
 
-function centerMap()
+function drawMap(map)
 {
+    var tableMap = document.getElementById("tableMap");
+    var row = tableMap.insertRow(0);
+    var colorsMap = {
+        'x': '#666666',
+        '1': '#ff6666',
+        '.': '#66cc00',
+        '2': '#6666ff'
+    };
+    for (var i = 0; i < map.length; i++)
+    {
+        var row = tableMap.insertRow(i);
+        var mapStr = [];
+        for (var j = 0; j < map.length; j++)
+        {
+            var cell = row.insertCell(j);
+            cell.style.backgroundColor = colorsMap[map[i][j]];
+            mapStr.push(map[i][j]);
+        }
+        map[i] = mapStr;
+    }
     var dw = $('#field').offset().left + ($('#field').width() - $('#fullMap').width())/2;
     var dh = $('#field').offset().top + ($('#field').height() - $('#fullMap').height())/2;
-    $('#fullMap').offset({top:dh, left:dw});
+    $('#fullMap').offset({top : dh, left : dw});
+    return map;
 }
 
-function changePos(posX, posY, length)
+function showUnits(map, unitsGame)
 {
-    if (posX == length) posX--;
-    if (posY == length) posY--;
+    /*each unit should have it's picture
+    var picUnit = {
+        'one': 'images/person1.bmp',
+        'two': 'images/person1.bmp',
+        'three': 'images/house.bmp'
+    };
+    */
+
+    var picUnit = ['images/person1.bmp','images/house.bmp','images/person1.bmp','images/house.bmp']
+
+    fragment = document.createDocumentFragment();
+    div = document.createElement('div');
+    div.className ='factDiv';
+    pic = document.createElement('img');
+    pic.width = 40;
+    for(var i = 0; i < unitsGame.length; i++)
+    {
+        for(var j = 0; j < unitsGame[i].count; j++)
+        {
+            //pic.src = picUnit[unitsGame[i].count];
+            pic.src = picUnit[i];
+            div.appendChild(pic);
+            var t = div.cloneNode(true);
+            t.id = unitsGame[i].name;
+            $(t).mousedown(function(){
+               changeCell(this,map);
+            });
+            fragment.appendChild(t);
+        }
+    }
+}
+
+function changePos(pos, len)
+{
+    if (pos == len) pos--;
+    return pos;
 }
